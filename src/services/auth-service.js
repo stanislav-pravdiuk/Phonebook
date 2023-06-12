b73ab536d80b8abfb30bc2650287f28174a4b8c5
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-06-12 16:41:42 +0300

    todo thunk

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -4,11 +4,25 @@ const instance = axios.create({
     baseURL: 'https://connections-api.herokuapp.com'
 });
 
+const setToken = (token) => {
+    instance.defaults.headers.common['Authorization'] = token;
+};
+
+export const delToken = () => {
+    delete instance.defaults.headers.common['Authorization'];
+};
+
 export const createUser = async (body) => {
     return await instance.post('/users/signup', body)
 };
 
 export const logInUser = async (body) => {
-    const { data } = await instance.post('/users/login', body)
+    const { data } = await instance.post('/users/login', body);
+    setToken(`Bearer ${data.token}`)
+    return data
+};
+
+export const getProfile = async () => {
+    const { data } = await instance('/users/current')
     return data
 };
