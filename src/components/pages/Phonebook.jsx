8be1ed3d3ded37f161b2ad523fd016599517b008
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-06-14 15:32:20 +0300

    add contactForm styles

--- src/components/pages/Phonebook.jsx
+++ src/components/pages/Phonebook.jsx
@@ -17,19 +17,8 @@ function Phonebook() {
     }, [dispatch])
     
     return (
-        <div
-            // style={{
-            //     height: '100vh',
-            //     display: 'flex',
-            //     flexDirection: 'column',
-            //     justifyContent: 'center',
-            //     alignItems: 'center',
-            //     fontSize: 40,
-            //     color: '#010101'
-            // }}
-        >
+        <div>
 
-        <h1>Phonebook</h1>
         <ContactForm />
 
         <h2>Contacts</h2>
--- src/components/phonebook/contactForm.jsx
+++ src/components/phonebook/contactForm.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
-import css from './contact-form.module.css';
 // import { useDispatch } from "react-redux";
 // import { postContactThunk } from "services/thunk";
 import { postContact } from "services/requestsAPI";
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
 
 function ContactForm() {
 
@@ -26,33 +39,71 @@ function ContactForm() {
     };
     
     return (
-        <form
-            className={css.contactForm}
-            onSubmit={handleSubmit}>
-            <label>Name
-                <input
+        <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <ContactPhoneIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+            Phonebook
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                    autoComplete="given-name"
+                    required
+                    fullWidth
+                    id="Name"
+                    label="Name"
+                    autoFocus
                     type="text"
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
                     value={state.name}
                     onChange={handleInputChange}
                 />
-            </label>
-            <label>Number
-                <input
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    id="tel"
+                    label="Number"
+                    autoComplete="tel"
                     type="tel"
                     name="number"
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
                     value={state.number}
                     onChange={handleInputChange}
                 />
-            </label>
-            <button type="submit">Add contact</button>
-        </form>
+                </Grid>
+            </Grid>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Add contact
+            </Button>
+            </Box>
+        </Box>
+
+        </Container>
+    </ThemeProvider>
+
     );
 };
 
