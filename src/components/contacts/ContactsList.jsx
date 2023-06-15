e680ad00b3a0c4faf8a473abe78819f8eeeac6bd
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-06-15 11:40:31 +0300

    add contact list styles

--- src/components/contacts/ContactsList.jsx
+++ src/components/contacts/ContactsList.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getFilteredContacts } from "redux/selectors";
 import { deleteContactThunk } from "services/thunk";
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
 
 function ContactList() {
 
@@ -10,17 +23,48 @@ function ContactList() {
 
         if (contacts.length !== 0) {
                 return (
-                        <ul>
-                                {contacts.map((el) =>
-                                        <li key={el.id}>
-                                                <span>{el.name} {el.number}</span>
-                                                <button onClick={() => dispatch(deleteContactThunk(el.id))}>Delete</button>
-                                        </li>)}
-                        </ul>
+
+        <ThemeProvider theme={defaultTheme}>
+                <CssBaseline />
+                <Container sx={{ py: 8 }} maxWidth="md">
+                        <Grid container spacing={4}>
+                                {contacts.map((el) => (
+                                        <Grid item key={el.id} xs={12} sm={6} md={4}>
+                                                <Card
+                                                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                                >
+                                                <CardMedia
+                                                        component="div"
+                                                        sx={{
+                                                                pt: '56.25%',
+                                                        }}
+                                                        image="https://source.unsplash.com/random?wallpapers"
+                                                />
+                                                <CardContent sx={{ flexGrow: 1 }}>
+                                                        <Typography gutterBottom variant="h5" component="h2">
+                                                                {el.name}
+                                                        </Typography>
+                                                        <Typography>
+                                                                {el.number}
+                                                        </Typography>
+                                                </CardContent>
+                                                <CardActions>
+                                                        <Button size="small" onClick={
+                                                                () => dispatch(deleteContactThunk(el.id))}>
+                                                                Delete
+                                                        </Button>
+                                                </CardActions>
+                                                </Card>
+                                        </Grid>
+                                ))}
+                        </Grid>
+                </Container>
+        </ThemeProvider>
                 );
         };
         
         return <p>No contacts found</p>
 };
+                
 
 export default ContactList;
