5051f7eb42ac48c12ad343d3fc28efdd868724e7
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-06-15 09:11:02 +0300

    add persistor

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import { store } from 'redux/store';
 import App from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react';
+import { persistor } from './redux/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/goit-react-hw-08-phonebook'>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,62 @@
+// import { configureStore } from "@reduxjs/toolkit";
+
+// import { itemsReducer } from "./contacts/contactsSlice";
+// import { filterReducer } from "./filter/filterSlice";
+// import { authReducer } from "./auth/authSlice";
+    
+// export const store = configureStore({
+//     reducer: {
+//         contacts: itemsReducer,
+//         filter: filterReducer,
+//         auth: authReducer
+//     },
+// });
+
+// -------------------------------add persistor
+
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
 
 import { itemsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filterSlice";
 import { authReducer } from "./auth/authSlice";
-    
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+    key: 'token',
+    storage,
+    // whitelist: ['token'],
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
 export const store = configureStore({
     reducer: {
         contacts: itemsReducer,
         filter: filterReducer,
-        auth: authReducer
+        auth: persistedReducer,
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ],
+            },
+        }),
 });
+
+export const persistor = persistStore(store)
